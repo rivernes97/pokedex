e0b52fb7440d04fad79392b15c81dcabc3898ebb
joaqu <joaqu@192.168.1.39>
2018-04-14 12:46:25 +0200

    NUEVOO

--- src/Pokemon/Pokemon.java
+++ src/Pokemon/Pokemon.java
@@ -9,6 +9,10 @@ public class Pokemon {
     public String nombre ="";
     public String altura ="";
     public String peso ="";
+    public String numero="";
+    public String felicidad ="";
+    public String experiencia="";
+    
     
     public int generation_id = 0;
     public int evolution_chain_id = 0;
--- src/Pokemon/VentanaPokedex.java
+++ src/Pokemon/VentanaPokedex.java
@@ -55,6 +55,10 @@ public class VentanaPokedex extends javax.swing.JFrame {
             jLabel1.setText(p.nombre);
             jLabel5.setText(p.peso);
             jLabel4.setText(p.altura);
+            jLabel6.setText(p.numero);
+            jLabel7.setText(p.experiencia);
+            jLabel8.setText(p.felicidad);
+            
         }
         else {
             jLabel1.setText("NO HAY DATOS");
@@ -83,6 +87,10 @@ public class VentanaPokedex extends javax.swing.JFrame {
                 p.species = resultadoConsulta.getString(12);
                 p.peso = resultadoConsulta.getString(11);
                 p.altura=resultadoConsulta.getString(10);
+                p.numero=resultadoConsulta.getString(1);
+                p.experiencia=resultadoConsulta.getString(18);
+                p.felicidad=resultadoConsulta.getString(19);
+                
                 listaPokemons.put(resultadoConsulta.getString(1), p);
             }
         }
@@ -107,9 +115,12 @@ public class VentanaPokedex extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -133,12 +144,25 @@ public class VentanaPokedex extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("High Tower Text", 1, 18)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 450, 120, 50));
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 150, 200, 170));
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 150, 200, 170));
+
+        jLabel4.setFont(new java.awt.Font("High Tower Text", 1, 18)); // NOI18N
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 240, 80, 20));
+
+        jLabel5.setFont(new java.awt.Font("High Tower Text", 1, 18)); // NOI18N
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 240, 80, 20));
+
+        jLabel6.setFont(new java.awt.Font("HP Simplified", 1, 32)); // NOI18N
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 20, 60, 50));
+
+        jLabel7.setFont(new java.awt.Font("Verdana Pro SemiBold", 1, 14)); // NOI18N
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 190, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Verdana Pro Black", 1, 14)); // NOI18N
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 190, -1, -1));
 
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Pokemon/Imagenes/pokedex1.png"))); // NOI18N
         getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(464, 240, 80, 20));
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(564, 240, 80, 20));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -201,5 +225,8 @@ public class VentanaPokedex extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     // End of variables declaration//GEN-END:variables
 }
